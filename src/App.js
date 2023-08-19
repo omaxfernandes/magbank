f28eef9e373086c19e75e189bc4e9731ba012a1d
maxsatos <max.sfernandes@outlook.com>
2023-08-19 16:34:30 -0300

    feat(components):Implemented hero

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 import "./App.scss";
-
-
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
