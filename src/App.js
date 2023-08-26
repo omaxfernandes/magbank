482567dc72a36e3932972762870ebe728f552882
max.santos <max.sfernandes@outlook.com>
2023-08-26 17:18:25 -0300

    feat(components): Implemented CreditCard, CardList, and CenteredButton components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import CreditCardComponet from "./components/CreditCard";
+import CardListComponent from "./components/CardList";
 import "./App.scss";
+import publicPosts from "./data/posts";
+import CenteredButton from "./components/CenteredButton";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <CreditCardComponet />
+      <CardListComponent publicPosts={publicPosts} />
+      <CenteredButton>Abra sua conta</CenteredButton>
     </div>
   );
 }
